3f73466982b98cec0ce3e604006c0198f45bb367
Junior Pinheiro <junior.main7@gmail.com>
2023-03-28 17:41:35 -0300

    algumas correções

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,16 @@ import { AuthGoogleContext } from '../../context/AuthGoogle';
 
 export default function Home() {
   const { user, signOut } = React.useContext(AuthGoogleContext);
-  
   console.log(user);
-
   return (
     <div>
       <h1>{user.displayName}</h1>
-      <img src={user.photoURL} alt="foto" />
       <button onClick={() => signOut()}>sair</button>
+      <img
+        src={user.photoURL}
+        alt="foto"
+        referrerpolicy="no-referrer"
+      />
     </div>
   );
 }
